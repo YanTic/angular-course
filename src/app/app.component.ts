8fb0ad08f84268dd6259b160af4fa5aec4b57e84
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2022-12-06 22:45:01 -0500

    Uso de ngIf, ngFor, push-splice y uso de interfaces para 'objetos'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-store';
+
+  items = ['nicolas', 'julian', 'sara'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'que lo que tu quiere mami'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'que lo que tu quiere mami'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'que lo que tu quiere mami'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'que lo que tu quiere mami'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'que lo que tu quiere mami'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'que lo que tu quiere mami'
+    }
+  ];
+
+  addItem() {
+    this.items.push('new item');
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }

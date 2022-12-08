a783bd9e53ccb9e6ee387e53a75d3fe6316699f4
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2022-12-07 23:11:58 -0500

    Uso de Componentes-decoradores-InputsyOutputs-CicloVida-Styles- y uso de ngGenerate y ngLint

---
+++ src/app/components/product.component.ts
@@ -0,0 +1,46 @@
+import {
+    Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, 
+    OnInit, DoCheck, OnDestroy } 
+from '@angular/core'
+
+import { Product } from '../product.model'
+
+@Component({
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.scss']
+    
+})
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
+    @Input()
+    product!: Product;
+
+    @Output()
+    productClicked: EventEmitter<any> = new EventEmitter();
+
+    constructor() {
+        console.log('1. Constructor');
+    }
+    
+    // ngOnChanges(changes: SimpleChanges): void {
+    //     console.log('2. ngOnChanges');
+    //     console.log(changes);
+    // }
+
+    ngOnInit(): void { //Aquí se empiezan a llamar las services
+        console.log('3. ngOnInit');
+    }
+
+    ngDoCheck(): void {
+        console.log('4. ngDoCheck');
+    }
+
+    ngOnDestroy(): void {
+        console.log('4. ngOnDestroy');
+    }
+
+    addCart() {
+        console.log('Añadir al carrito');
+        this.productClicked.emit(this.product.id);
+    }
+}

ce9265c14b35221b3f481f6016a326b35e95f607
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2022-12-12 22:39:34 -0500

    Uso de swiper para un carrusel

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,26 @@
+import { Component, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent implements AfterViewInit {
+
+  mySwiper!: Swiper;
+
+  images: string[] = [
+    'assets/images/banner-1.jpg',
+    'assets/images/banner-2.jpg',
+    'assets/images/banner-3.jpg'
+  ];
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.mySwiper = new Swiper('.swiper-container');
+  }
+}
